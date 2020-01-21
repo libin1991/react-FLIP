7663f4d6ed51500a5ea5d139c03a30e93933a816
libin40 <libin40@baidu.com>
2020-01-21 16:35:01 +0800

    add sdk desc

---
+++ build/precache-manifest.a9280e4ef13111bfe0394ad4127ff976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cf7a677030317f2b6a6517a4b6dc46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddbfc541b827af03c4a3",
+    "url": "/static/css/main.9a090002.chunk.css"
+  },
+  {
+    "revision": "8a02d7fd010068d5d74d",
+    "url": "/static/js/2.8731490d.chunk.js"
+  },
+  {
+    "revision": "ddbfc541b827af03c4a3",
+    "url": "/static/js/main.799c3eae.chunk.js"
+  },
+  {
+    "revision": "57a84b8fda7dd25ae783",
+    "url": "/static/js/runtime~main.455ba8ae.js"
+  }
+]);
